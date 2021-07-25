04f77b860dd06e09304074129655769e48ee4df5
Alichatraei <avida.chatraee2001@yahoo.com>
2021-07-25 11:23:56 +0430

    Add logo & button to navbar

--- src/Layout/HeaderLayout.tsx
+++ src/Layout/HeaderLayout.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Logo from "../Assets/Images/Logo.svg";
-import { AppBar, Toolbar, Button, CardMedia } from "@material-ui/core";
+import { AppBar, Toolbar, Button } from "@material-ui/core";
 import useStyles from "./Styles/Styles";
 const HeaderLayout = () => {
   const classes: any = useStyles();
   return (
     <AppBar position="static" className={classes.header}>
-      <Toolbar>
-        <CardMedia image={Logo} src={Logo} />
-        <Button color="inherit">ورود</Button>
+      <Toolbar className={classes.toolbar}>
+        <div className={classes.logoDiv}>
+          <img src={Logo} alt={Logo} className={classes.logoImage} />
+        </div>
+        <Button color="inherit" className={classes.Button}>
+          ورود
+        </Button>
       </Toolbar>
     </AppBar>
   );
--- src/Layout/Styles/Styles.ts
+++ src/Layout/Styles/Styles.ts
@@ -3,10 +3,31 @@ const useStyles = makeStyles((theme: Theme): any => {
   return createStyles({
     header: {
       flexGrow: 1,
-      minHeight: "10vh",
-      backgroundColor: "red",
+      backgroundColor: "white",
+      height: "auto",
+      padding: "1rem",
       display: "flex",
       alignItems: "center",
+    },
+    toolbar: {
+      width: "100%",
+      padding: "0",
+      justifyContent: "space-between",
+    },
+    logoDiv: {
+      width: "100%",
+      height: "100%",
+      display: "flex",
+      justifyContext: "start",
+      alignItems: "center",
+    },
+    logoImage: {
+      width: "auto",
+      height: "100%",
+    },
+    Button: {
+      color: "#ccc",
+      padding: "1rem",
     },
   });
 });
