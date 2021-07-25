93dfbf2a811854bf43d217cea68de75f908528c1
Alichatraei <avida.chatraee2001@yahoo.com>
2021-07-25 10:28:32 +0430

    Add config for svg file

--- src/Layout/HeaderLayout.tsx
+++ src/Layout/HeaderLayout.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Logo from "../Assets/Images/Logo.svg";
 import { AppBar, Toolbar, Button } from "@material-ui/core";
 const HeaderLayout = () => {
   return (
     <AppBar position="static">
       <Toolbar>
         <div>
-          <img src="" alt="" />
+          <img src={Logo} alt="Logo" />
         </div>
         <Button color="inherit">Login</Button>
       </Toolbar>
