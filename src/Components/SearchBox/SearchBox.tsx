7138731735962ca5a120d699d0e482ecfd32b37a
Alichatraei <avida.chatraee2001@yahoo.com>
2021-07-26 09:20:51 +0430

    Remove Material UI from Project

--- src/Components/SearchBox/SearchBox.tsx
+++ src/Components/SearchBox/SearchBox.tsx
@@ -1,44 +1,15 @@
-import { Popover, Typography } from "@material-ui/core";
+import { InputBase } from "@material-ui/core";
 import React from "react";
-import { useState } from "react";
-
-const SearchBox = () => {
-  const [anchorE1, setAnchorE1] = useState<HTMLElement | null>(null);
-  const handlePopoverOpen = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
-    setAnchorE1(e.currentTarget);
-  };
-  const handlePopoverClose = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
-    setAnchorE1(null);
-  };
-  const open = Boolean(anchorE1);
+import SearchBoxStyles from "./SearchBox.Styles";
+const SearchBox = (): JSX.Element => {
+  const classes: any = SearchBoxStyles();
   return (
-    <div className="searchBox" style={{ height: "10rem" }}>
-      <Typography
-        style={{ height: "10rem" }}
-        aria-owns={open ? "mouse-over-popover" : undefined}
-        aria-haspopup="true"
-        onMouseEnter={handlePopoverOpen}
-      >
-        روی این متن هاور کن
-      </Typography>
-      <Popover
-        style={{
-          pointerEvents: "none",
-        }}
-        open={open}
-        anchorEl={anchorE1}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "center",
-        }}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "center",
-        }}
-        onMouseLeave={handlePopoverClose}
-      >
-        این منو است
-      </Popover>
+    <div className={classes.searchBox}>
+      <InputBase
+        placeholder="دنبال چی میگردی"
+        className={classes.inputBase}
+        autoFocus
+      />
     </div>
   );
 };
--- src/Layout/HeaderLayout.tsx
+++ src/Layout/HeaderLayout.tsx
@@ -1,25 +1,9 @@
 import React from "react";
 import Logo from "../Assets/Images/Logo.svg";
 import { SearchBox } from "../Components";
-import { AppBar, Toolbar, Button } from "@material-ui/core";
-import useStyles from "./Styles/Styles";
+
 const HeaderLayout = () => {
-  const classes: any = useStyles();
-  return (
-    <AppBar position="static" className={classes.header}>
-      <Toolbar className={classes.toolbar}>
-        <div className={classes.logoDiv}>
-          <img src={Logo} alt={Logo} className={classes.logoImage} />
-        </div>
-        <div className={classes.searchBox}>
-          <SearchBox />
-        </div>
-        <Button color="inherit" className={classes.Button}>
-          ورود
-        </Button>
-      </Toolbar>
-    </AppBar>
-  );
+  return <></>;
 };
 
 export default HeaderLayout;
