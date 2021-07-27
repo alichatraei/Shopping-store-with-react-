98e1877d5e697ac22d497a87338eaf3b3f48e57f
Alichatraei <avida.chatraee2001@yahoo.com>
2021-07-27 08:50:59 +0430

    Add header icons box

--- src/Components/SearchMenuBox/SearchMenuBox.tsx
+++ src/Components/SearchMenuBox/SearchMenuBox.tsx
@@ -1,13 +1,24 @@
 import React from "react";
-import SearchMenuBoxStyles from "./SearchMenuBox.module.css";
 import { DropDownMenu, SearchInput } from "..";
+import { Container, Row, Col } from "react-bootstrap";
 import { FiSearch } from "react-icons/fi";
+import SearchMenuBoxStyles from "./SearchMenuBox.module.css";
 const SearchMenuBox = (): JSX.Element => {
   return (
     <div className={SearchMenuBoxStyles.searchMenuContainer}>
-      <DropDownMenu />
-      <SearchInput />
-      <FiSearch className="h-100" color="#818181" />
+      <Container fluid className="h-100 w-100 p-0">
+        <Row className="h-100 align-items-center w-100 m-0 justify-md-content-evenly ">
+          <Col className="h-100 d-flex align-items-center p-0 w-auto">
+            <DropDownMenu />
+          </Col>
+          <Col className="h-100 p-0">
+            <SearchInput />
+          </Col>
+          <Col className="h-100 d-none d-md-flex p-0 justify-content-end float-left">
+            <FiSearch className="h-100" color="#818181" />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
--- src/Layout/HeaderLayout/HeaderLayout.tsx
+++ src/Layout/HeaderLayout/HeaderLayout.tsx
@@ -3,18 +3,32 @@ import Logo from "../../Assets/Images/Logo.svg";
 import { Container, Col, Row } from "react-bootstrap";
 import HeaderLayoutStyles from "./HeaderLayout.module.css";
 import { SearchMenuBox } from "../../Components";
+import heart from "../../Assets/Icons/heart.svg";
+import shoppingCart from "../../Assets/Icons/shopping-cart.svg";
+import userProfile from "../../Assets/Icons/user.svg";
 const HeaderLayout = () => {
   return (
     <header className={HeaderLayoutStyles.header}>
       <Container fluid className="h-100">
-        <Row className="h-100">
-          <Col className="h-100">
+        <Row className="h-100 justify-content-between">
+          <Col xs={12} md={2} lg={2} className="h-100">
             <img src={Logo} alt={Logo} title={Logo} className="h-100" />
           </Col>
-          <Col className="h-100 d-flex align-items-center">
+          <Col
+            xs={12}
+            sm={6}
+            className="h-100 d-flex align-items-center justify-content-center"
+          >
             <SearchMenuBox />
           </Col>
-          <Col></Col>
+          <Col
+            md={2}
+            className="d-none d-md-flex h-100 align-items-center justify-content-between"
+          >
+            <img src={userProfile} alt="userProfile" className="h-50" />
+            <img src={heart} alt="heart" className="h-50" />
+            <img src={shoppingCart} alt="shoppingCart" className="h-50" />
+          </Col>
         </Row>
       </Container>
     </header>
