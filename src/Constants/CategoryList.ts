59a498bb08602a5f93d661115a127b9ab8b36c68
Alichatraei <avida.chatraee2001@yahoo.com>
2021-07-28 01:33:52 +0430

    Add some categories and their icons

--- src/Constants/CategoryList.ts
+++ src/Constants/CategoryList.ts
@@ -1,10 +1,20 @@
-type TCategoryList = string[];
+import { RiStore3Line } from 'react-icons/ri'
+import { IoPhonePortraitSharp, IoShirtOutline, IoHomeOutline, IoBookOutline } from 'react-icons/io5'
+import { GiLipstick } from 'react-icons/gi'
+type TCategoryList = [
+    { en: string, fa: string, icon: any },
+    { en: string, fa: string, icon: any },
+    { en: string, fa: string, icon: any },
+    { en: string, fa: string, icon: any },
+    { en: string, fa: string, icon: any },
+    { en: string, fa: string, icon: any },
+]
 const CategoryList: TCategoryList = [
-    'سوپرمارکت',
-    'تلفن همراه',
-    'پوشاک',
-    'آرایش و بهداشتی',
-    'خانه و آشپزخانه',
-    'کتاب و هنر'
+    { en: "superMarket", fa: 'سوپرمارکت', icon: RiStore3Line },
+    { en: "phone", fa: 'تلفن همراه', icon: IoPhonePortraitSharp },
+    { en: "clothes", fa: 'پوشاک', icon: IoShirtOutline },
+    { en: "comestic", fa: 'لوازم آرایشی و بهداشتی', icon: GiLipstick },
+    { en: "home", fa: 'خانه و آشپزخانه', icon: IoHomeOutline },
+    { en: "book", fa: 'کتاب و هنر', icon: IoBookOutline },
 ]
 export default CategoryList
