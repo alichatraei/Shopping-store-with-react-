2438899a8a949aac103d758b3dc9571c3db32320
Alichatraei <avida.chatraee2001@yahoo.com>
2021-07-27 17:17:23 +0430

    Create banners container

---
+++ src/Components/BannersContainer/BannersContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+interface IProps {
+    children: React.ReactNode
+}
+const BannersContainer: React.FC<IProps> = ({ children }) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default BannersContainer
