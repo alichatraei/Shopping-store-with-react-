2c08d9567d83aead0105cf3b99ec74d8a5938482
Alichatraei <avida.chatraee2001@yahoo.com>
2021-07-28 18:51:41 +0430

    Complete footer feature item and container

--- src/Components/FooterFeatureItem/FooterFeatureItem.tsx
+++ src/Components/FooterFeatureItem/FooterFeatureItem.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import FooterFeatureInformation from '../../Constants/FooterFeatureInformation'
+import FooterFeatureItemStyles from './FooterFeatureItemStyles.module.css'
 const FooterFeatureItem = () => {
     return (<>
         {
             FooterFeatureInformation.map((item, index) => {
                 return (
-                    < div className="h-auto col-12 col-sm-6 col-md-3" key={index + 1} >
-                        <div className="featureIcon h-auto">
+                    < div className={`h-auto col-12 col-sm-6 col-md-3
+                    d-flex justify-content-center aling-items-center
+                    flex-column
+                    mb-5 mb-md-0 ${FooterFeatureItemStyles.footerFeatureCard}`} key={index + 1} >
+                        <div className="featureIcon h-auto text-center">
                             <img src={item.icon_path} alt={item.title} title={item.title} />
                         </div>
                         <div className="featureTitle h-auto">
-                            <h4 className="h-auto">{item.title}</h4>
+                            <h5 className="h-auto text-center mb-3">{item.title}</h5>
                         </div>
                         <div className="featureSubtitle h-auto">
-                            <h6>{item.subtitle}</h6>
+                            <h6 className="text-center">{item.subtitle}</h6>
                         </div>
                     </div >
                 )
--- src/Components/FooterFeaturesContainer/FooterFeaturesContainer.tsx
+++ src/Components/FooterFeaturesContainer/FooterFeaturesContainer.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { FooterFeatureItem } from '..'
+import FooterFeaturesContainerStyles from './FooterFeaturesContainerStyles.module.css'
 const FooterFeaturesContainer = () => {
     return (
-        <article className="h-auto container">
-            <div className="row h-auto">
+        <article className={`
+        container 
+        ${FooterFeaturesContainerStyles.featureContainer}`}>
+            <div className="row h-auto justify-content-center">
                 <FooterFeatureItem />
             </div>
         </article>
