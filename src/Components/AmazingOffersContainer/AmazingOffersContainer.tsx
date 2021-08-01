9da28cd73f7fc3ecb800b0e6d92e591f6c063d14
Alichatraei <avida.chatraee2001@yahoo.com>
2021-08-01 11:07:17 +0430

    Create amazing offers container component

---
+++ src/Components/AmazingOffersContainer/AmazingOffersContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import AmazingOffersContainerStyles from './AmazingOffersContainerStyles.module.css'
+const AmazingOffersContainer = () => {
+    return (
+        <div className={AmazingOffersContainerStyles.amazingOffersContainer}>
+
+        </div>
+    )
+}
+
+export default AmazingOffersContainer
