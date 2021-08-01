2d632fd86179aeb8d46122ee2bd05cf6f8c02eec
Alichatraei <avida.chatraee2001@yahoo.com>
2021-08-01 17:39:17 +0430

    Import Swiper JS library and complete amazing offers card

--- src/Components/AmazingOffersCard/AmazingOffersCard.tsx
+++ src/Components/AmazingOffersCard/AmazingOffersCard.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
+import amazingOffersImage from '../../Assets/Images/AmazingOffers.png'
 import AmazingOffersCardStyles from './AmazingOffersCardStyles.module.css'
 const AmazingOffersCard = () => {
     return (
         <div className={AmazingOffersCardStyles.amazingOffers}>
             <div className={AmazingOffersCardStyles.imageOffer}>
-                <img src="#!" alt="" />
+                <img src={amazingOffersImage} alt="پیشنهاد شگفت انگیز" />
             </div>
             <div className={AmazingOffersCardStyles.beforeOffer}>
-                <span className={AmazingOffersCardStyles.beforeOfferPrice}>۷۶۰,۰۰۰ تومان</span>
                 <span className={AmazingOffersCardStyles.offerPercent}>۱۲٪</span>
+                <span className={AmazingOffersCardStyles.beforeOfferPrice}>۷۶۰,۰۰۰ تومان</span>
             </div>
             <div className={AmazingOffersCardStyles.afterOffer}>
                 <span>۶۵۰,۰۰۰ تومان</span>
             </div>
-            <div className="productName">
+            <div className={AmazingOffersCardStyles.productName}>
                 <span>لنز هشتایی گوشی موبایل</span>
             </div>
         </div >
