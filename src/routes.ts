fc94ff8fb3c320c26200b35fa317cf48f2dc12ca
Alichatraei <avida.chatraee2001@yahoo.com>
2021-08-01 16:54:41 +0430

    Add category detail route to routes file

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,15 @@
 import React from 'react'
 import RoutesType from "./Types/RoutesType";
 const Home = React.lazy(() => import('./Views/Home/Home'))
+const CategoryDetail = React.lazy(() => import('./Views/CategoryDetail/CategoryDetail'))
 const Routes: RoutesType = [{
     path: '/',
     exact: true,
     page: Home
+},
+{
+    path: '/categoryDetail',
+    exact: true,
+    page: CategoryDetail
 }]
 export default Routes
