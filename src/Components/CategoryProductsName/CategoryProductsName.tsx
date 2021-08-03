b7acb87172089c929d9da7ba41dce1d8f0a8e729
Alichatraei <avida.chatraee2001@yahoo.com>
2021-08-03 17:41:23 +0430

    Make category products name responsive

--- src/Components/CategoryProductsName/CategoryProductsName.tsx
+++ src/Components/CategoryProductsName/CategoryProductsName.tsx
@@ -3,9 +3,15 @@ import CategoryProductIcon from '../../Assets/Icons/categoryProductIcon.svg'
 import CategoryProductsNameStyles from './CategoryProductsName.module.css'
 const CategoryProductsName = () => {
     return (
-        <div className="w-100">
-            <img src={CategoryProductIcon} alt="Icon" />
-            <span className={CategoryProductsNameStyles.categoryName}>{"لوازم جانبی موبایل"}</span>
+        <div className="w-100 container p-0">
+            <div className="row">
+                <div className="col-1">
+                    <img src={CategoryProductIcon} alt="Icon" />
+                </div>
+                <div className="col p-0">
+                    <span className={CategoryProductsNameStyles.categoryName}>{"لوازم جانبی موبایل"}</span>
+                </div>
+            </div>
         </div>
     )
 }
