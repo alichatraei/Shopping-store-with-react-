45503f2f58f6cdc3f3d840e28d5e19b29b178eab
Alichatraei <avida.chatraee2001@yahoo.com>
2021-08-03 18:23:36 +0430

    Edit width of Product Item

--- src/Components/ProductItemSlider/ProductItemSlider.tsx
+++ src/Components/ProductItemSlider/ProductItemSlider.tsx
@@ -7,12 +7,12 @@ const ProductItemSlider = () => {
             <div className={`${ProductItemSliderStyles.productImage}`}>
                 <img
                     src="https://dkstatics-public.digikala.com/digikala-products/115598446.jpg?x-oss-process=image/resize,m_lfit,h_350,w_350/quality,q_60"
-                    alt="photo1" className="h-auto" />
+                    alt="photo1" className="w-100 h-100" />
             </div>
-            <div className="productName h-auto text-center">
+            <div className="productName p-3 h-auto text-center">
                 <h5 className="h-auto">گوشی هوشمند</h5>
             </div>
-            <div className="productPrice w-100 h-auto">
+            <div className="productPrice p-3 w-100 h-auto">
                 <h6 className="h-auto">قیمت</h6>
                 <div className="h-auto d-flex justify-content-between"><span className="h-auto">۲,۳۰۰,۰۰۰ تومان</span>
                     <BiHeart className="h-auto" size="2rem" color="#FF7C7C" />
