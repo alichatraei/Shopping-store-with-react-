f3b2c738b4e0ca864f963f4f1df751f428ebc48d
Alichatraei <avida.chatraee2001@yahoo.com>
2021-08-03 11:05:24 +0430

    Create brand lists component for accordion filter box

---
+++ src/Components/BrandListsAccordionFilterBox/BrandListsAccordionFilterBox.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BrandListsAccordionFilterBox = () => {
+    return (
+        <div className='d-flex align-items-center justify-content-between'>
+            <label htmlFor="checkBox">ال جی</label>
+            <input type="checkbox" name="brandName" id="checkBox" />
+        </div>
+    )
+}
+
+export default BrandListsAccordionFilterBox
