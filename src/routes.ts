3ca2e49b59ba6c5dcc3f512734b707bb1864ac45
Alichatraei <avida.chatraee2001@yahoo.com>
2021-08-04 09:32:29 +0430

    Import product detail view to routes array

--- src/routes.ts
+++ src/routes.ts
@@ -2,14 +2,20 @@ import React from 'react'
 import RoutesType from "./Types/RoutesType";
 const Home = React.lazy(() => import('./Views/Home/Home'))
 const CategoryDetail = React.lazy(() => import('./Views/CategoryDetail/CategoryDetail'))
+const ProductDetail = React.lazy(() => import('./Views/ProductDetail/ProductDetail'))
 const Routes: RoutesType = [{
     path: '/',
     exact: true,
     page: Home
 },
 {
-    path: '/categoryDetail',
+    path: '/categorydetail',
     exact: true,
     page: CategoryDetail
+},
+{
+    path: '/productdetail',
+    exact: true,
+    page: ProductDetail
 }]
 export default Routes
