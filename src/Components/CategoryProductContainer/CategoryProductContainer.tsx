52ddfc8f7c432686d09d61fc5f6e601d1e076de9
Alichatraei <avida.chatraee2001@yahoo.com>
2021-08-04 09:32:01 +0430

    Create product detail view

--- src/Components/CategoryProductContainer/CategoryProductContainer.tsx
+++ src/Components/CategoryProductContainer/CategoryProductContainer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {
     CategoryProductsName, CategoryProductSortGroup,
     CategoryProductsHashtags,
-    CategoryProductsContainer
+    CategoryProducts
 } from '../'
 import CategoryProductContainerStyles from './CategoryProductContainer.module.css'
 const CategoryProductContainer = () => {
@@ -11,7 +11,7 @@ const CategoryProductContainer = () => {
             <CategoryProductsName />
             <CategoryProductSortGroup />
             <CategoryProductsHashtags />
-            <CategoryProductsContainer />
+            <CategoryProducts />
         </section>
     )
 }
---
+++ src/Views/ProductDetail/ProductDetail.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ProductDetailStyles from './ProductDetail.module.css'
+const ProductDetail = () => {
+    return (
+        <div>
+            Product Detail
+        </div>
+    )
+}
+
+export default ProductDetail
